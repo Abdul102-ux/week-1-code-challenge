514312cd365652ce63acea7268487d0557f04e4d
abdul khaliq <abdallamakhtal@gmail.com>
2024-06-30 23:15:07 +0300

    I have created challenge2 .js file

---
+++ challenge2/challenge2 .js
@@ -0,0 +1,16 @@
+// solutions for challenge2.js
+function speedDetector(speed) {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if (speed <= speedLimit) {
+        console.log("Ok");
+    } else {
+        const points = Math.floor((speed - speedLimit) / kmPerPoint);
+        if (points >= 12) {
+            console.log("License suspended");
+        } else {
+            console.log(`Points: ${points}`);
+        }
+    }
+}
