a5ad20515f769f1f3e9f07bd26f8cd18978880d2
abdul khaliq <abdallamakhtal@gmail.com>
2024-06-30 22:42:01 +0300

    I have created challenge1 js file

---
+++ solutions/challenge2 .js
@@ -0,0 +1,15 @@
+function speedDetector(speed) {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if (speed <= speedLimit) {
+        return "Ok";
+    } else {
+        const points = Math.floor((speed - speedLimit) / kmPerPoint);
+        if (points >= 12) {
+            return "License suspended";
+        } else {
+            return `Points: ${points}`;
+        }
+    }
+}
