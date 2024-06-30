ac0240032f404d5f66450a8b6cd764e6d8ac5cc4
abdul khaliq <abdallamakhtal@gmail.com>
2024-06-30 23:19:42 +0300

    I have created challenge3 .js file

---
+++ challenge3/challenge3 .js
@@ -0,0 +1,15 @@
+// solutions/challenge3.js
+function calculateNetSalary(basicSalary, benefits) {
+    const kraTaxRate = 0.3;
+    const nhifRate = 0.05;
+    const nssfRate = 0.06;
+
+    let grossSalary = basicSalary + benefits;
+    let payee = grossSalary * kraTaxRate;
+    let nhifDeduction = grossSalary * nhifRate;
+    let nssfDeduction = grossSalary * nssfRate;
+    let netSalary = grossSalary - payee - nhifDeduction - nssfDeduction;
+
+    console.log(`Gross Salary: ${grossSalary}`);
+    console.log(`Net Salary: ${netSalary}`);
+}
